4c1c23c51c28a610993ec650f9d54cf61164f17c
Masna673450197-0 <noraphat200548@gmail.com>
2024-11-26 11:34:18 +0700

    masbit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("name Noraphat Srithep");
+Console.WriteLine("Ny student id: 673450197-0");
+string is DataType--> ชนิดของข้อมูล
+name is mas
+
